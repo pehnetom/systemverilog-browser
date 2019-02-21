a7f5de52ab82c48ae1b6bdfbb2b3daaebd4e9117
Pehnelt, Tomáš, Ing <pehnetom@fel.cvut.cz>
2019-02-21 21:38:09 +0100

    Initial commit.

---
+++ spec/systemverilog-browser-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import SystemverilogBrowser from '../lib/systemverilog-browser';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('SystemverilogBrowser', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('systemverilog-browser');
+  });
+
+  describe('when the systemverilog-browser:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.systemverilog-browser')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'systemverilog-browser:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.systemverilog-browser')).toExist();
+
+        let systemverilogBrowserElement = workspaceElement.querySelector('.systemverilog-browser');
+        expect(systemverilogBrowserElement).toExist();
+
+        let systemverilogBrowserPanel = atom.workspace.panelForItem(systemverilogBrowserElement);
+        expect(systemverilogBrowserPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'systemverilog-browser:toggle');
+        expect(systemverilogBrowserPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.systemverilog-browser')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'systemverilog-browser:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let systemverilogBrowserElement = workspaceElement.querySelector('.systemverilog-browser');
+        expect(systemverilogBrowserElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'systemverilog-browser:toggle');
+        expect(systemverilogBrowserElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/systemverilog-browser-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SystemverilogBrowserView from '../lib/systemverilog-browser-view';
+
+describe('SystemverilogBrowserView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
